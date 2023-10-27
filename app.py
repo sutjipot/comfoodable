0f1323ece1918f5b4638addaeb8c4164126a9e92
sutjipot <137701637+sutjipot@users.noreply.github.com>
2023-10-28 00:10:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ import streamlit as st
 from PIL import Image
 
 def main():
+    st.markdown(
+        """
+        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
+            <h1 style="text-align: center;">*Comfoodable*</h1>
+            <img src="data/food.jpg" style="width: 50%; height: auto;">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
     image = Image.open("data/food.jpg")
     st.image(image)
-    st.markdown("# *Comfoodable*")
+    
 
 if __name__ == "__main__":
     main()
