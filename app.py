2191d4b1e3e99c947fa421bcfe9efc117d9dcb7f
sutjipot <137701637+sutjipot@users.noreply.github.com>
2023-10-28 01:20:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from PIL import Image
-import SessionState
 
 def main():
     st.markdown("# *:pizza::ramen: Comfoodable :bread::fork_and_knife:*")
@@ -11,20 +10,16 @@ def main():
     st.markdown("I have a few ingredients at home.. I barely have time, but I have to cook! Let's use the tool below! :relieved:")
     st.text("")
 
-    select = st.selectbox("Choose your dietary preference!", ["No restriction", "Vegan", "vegetarian" ,"Dairy-free", "Gluten-free"])
 
-    session_state = SessionState.get(
-        recipe_df = "",
-        recipes="",
-        model_computed=False,
-        execute_recsys=False,
-        recipe_df_clean="",
-    )
+    col1, buff, col2 = st.beta_columns(1,1,6)
 
-    ingredients = st.text_input(
-        "Enter ingredients you would like to cook with (seperate ingredients with a comma)",
-        "chinese five spices, sirloin steak, feta cheese, bacon",
-    )
+    with col1:
+        select = st.selectbox("Choose your dietary preference!", ["No restriction", "Vegan", "vegetarian" ,"Dairy-free", "Gluten-free"])
+    with col2:
+        ingredients = st.text_input(
+            "Enter ingredients you would like to cook with (seperate ingredients with a comma)",
+            "chinese five spices, sirloin steak, feta cheese, bacon",
+        )
 
 if __name__ == "__main__":
     main()
