0e5eef3af95bca329ba019021b7c6a9d62c1ce53
sutjipot <137701637+sutjipot@users.noreply.github.com>
2023-10-28 00:11:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from PIL import Image
 def main():
     st.markdown(
         """
-        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
-            <h1 style="text-align: center;">*Comfoodable*</h1>
-            <img src="data/food.jpg" style="width: 50%; height: auto;">
+        <div style="justify-content: center; align-items: center; height: 100vh;">
+            <h1 style="text-align: center;">Comfoodable</h1>
         </div>
         """,
         unsafe_allow_html=True
