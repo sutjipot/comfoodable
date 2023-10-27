f0370076e03312c4e65e566e0a087435038cab71
sutjipot <137701637+sutjipot@users.noreply.github.com>
2023-10-28 00:26:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 from PIL import Image
+from streamlit_chat import message
 
 def main():
     st.markdown("# *:pizza::ramen: Comfoodable :bread::fork_and_knife:*")
+    st.markdown("A content-based filtering ML for recipe recommendations by Indira Sutjipto and Ilona Nikkar")
     image = Image.open("data/food.jpg")
-    st.image(image, height=400)
+    st.image(image)
+    st.markdown("## What should I make today? :yum:")
+    message("I have a few ingredients at home.. I barely have time, but I have to cook!", is_user = True)
     
 
 if __name__ == "__main__":
