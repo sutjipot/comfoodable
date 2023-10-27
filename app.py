7bfdf3a314a6a46f4c6901e6d119ac45bb844cb0
sutjipot <137701637+sutjipot@users.noreply.github.com>
2023-10-28 00:01:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 def main():
+    image = Image.open("data/food.jpg").resize((680, 150))
+    st.image(image)
     st.markdown("# *Comfoodable*")
 
 if __name__ == "__main__":
