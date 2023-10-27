8db49a0a4f95b67295de68c7c14b9d45bec32398
sutjipot <137701637+sutjipot@users.noreply.github.com>
2023-10-28 02:12:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 from PIL import Image
+from word2vec_dairyfree import get_df_recs
+from word2vec_glutenfree import get_gf_recs
+from word2vec_mains import get_mains_recs
+from word2vec_vegan import get_vegan_recs
+from word2vec_vegetarian import get_vegetarian_recs
+
+
+def clickable(name, link):
+    return f'<a target="_blank" href="{link}">{name}</a>'
+
 
 def main():
     st.markdown("# *:pizza::ramen: Comfoodable :bread::fork_and_knife:*")
@@ -10,6 +20,13 @@ def main():
     st.markdown("I have a few ingredients at home.. I barely have time, but I have to cook! Let's use the tool below! :relieved:")
     st.text("")
 
+    if "execute" not in st.session_state:
+        st.session_state.execute = False
+        st.session_state.computed = False
+        st.session_state.recipes = ""
+        st.session_state.clean = ""
+    
+    
     col1, col2 = st.columns([1.8,4])
     with col1:
         select = st.selectbox("Choose dietary preference!", ["No restriction", "Vegan", "Vegetarian" ,"Dairy-free", "Gluten-free"], placeholder="Select")
@@ -20,20 +37,23 @@ def main():
             "chinese five spices, sirloin steak, feta cheese, bacon",)
         st.session_state.ingredients = ingredients
     st.text("")
-    search = st.button("Search recipes that match!")
+    st.session_state.execute = st.button("Search recipes that match!")
 
-    if search:
-        if select and ingredients:
-            if select == "No restriction":
-                st.write("yeyeye")
-            elif select == "Vegan":
-                pass
-            elif select == "Vegetarian":
-                pass
-            elif select == "Gluten-free":
-                pass
-            else:
-                pass
+    if st.session_state.execute:
+        if select == "No restriction":
+            col1, col2, col3, col4, col5 = st.columns([1,1,3,1,1])
+            recipe = get_mains_recs(ingredients, mean=True)
+            with col1:
+                st.write(recipe)
+        
+        elif select == "Vegan":
+            pass
+        elif select == "Vegetarian":
+            pass
+        elif select == "Gluten-free":
+            pass
+        else:
+            pass
 
 
 
