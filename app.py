b8b6f45a52f2730630ea617a1917962ee7d33254
sutjipot <137701637+sutjipot@users.noreply.github.com>
2023-10-28 00:18:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from PIL import Image
 
 def main():
-    st.markdown("# *Comfoodable :bread::fork_and_knife:*")
+    st.markdown("# *:pizza::ramen: Comfoodable :bread::fork_and_knife:*")
     image = Image.open("data/food.jpg")
-    st.image(image)
+    st.image(image, height=400)
     
 
 if __name__ == "__main__":
